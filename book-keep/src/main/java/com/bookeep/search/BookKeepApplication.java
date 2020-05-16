4625c5aabc46189fcd39ba0da89cef0e058979f5
Prateek Tiwari <prateekt91@gmail.com>
2020-05-17 02:25:27 +0530

    Initial Commit for BookRepo Service

---
+++ book-keep/src/main/java/com/bookeep/search/BookKeepApplication.java
@@ -0,0 +1,13 @@
+package com.bookeep.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookKeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookKeepApplication.class, args);
+	}
+
+}
---
+++ book-keep/src/test/java/com/bookeep/search/BookKeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookeep.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookKeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
